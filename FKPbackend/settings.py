4361ff2c5013a8752edabb28a296079f9e522c39
Kosma Cieliczko <kosmacieliczko@Kosmas-MacBook-Air.local>
2024-07-02 12:47:22 +0200

    added code for pythonanywhere

--- FKPbackend/settings.py
+++ FKPbackend/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +28,9 @@ SECRET_KEY = "django-insecure-xpf-lom7$pzupcb64ut%j-k295#35lkj=nbaenf(rmmhtd*zvn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "kosmacieliczko.pythonanywhere.com",
+    "127.0.0.1",]
 
 
 # Application definition
@@ -42,11 +47,12 @@ INSTALLED_APPS = [
     "organization",
     "partners",
     "MonumentType",
-    "monument"
+    "monument",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join("/home/KosmaCieliczko/FKPbackend/staticfiles", "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
